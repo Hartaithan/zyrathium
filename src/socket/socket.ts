cd3470eab9a32f68af97959376465f10ea5aa855
Hartaithan <hartaithan@gmail.com>
2025-08-14 14:10:48 +0900

    store previous connections and listen on reconnect events

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -6,10 +6,20 @@ class InterceptedWS extends OriginalWS {
   constructor(...args: SocketParams) {
     super(...args);
     console.log("socket intercepted", args[0] || "url not found");
+
+    this.addEventListener("open", () => {
+      console.log("new socket connection is open", args[0]);
+    });
+
+    this.addEventListener("close", (event) => {
+      console.log("socket connection is closed", args[0], event);
+    });
+
     window.interceptedWS = this;
+
+    if (!window.allInterceptedWS) window.allInterceptedWS = [];
+    window.allInterceptedWS.push(this);
   }
 }
-
-InterceptedWS.prototype = OriginalWS.prototype;
 
 window.WebSocket = InterceptedWS;
